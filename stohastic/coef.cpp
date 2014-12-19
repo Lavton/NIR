e4ca83f7e56605a1990bf8d337abac1f16328e8d
Lioznov Anton <Lavton@gmail.com>
2014-12-19 16:03:20 +0300

    with x from t

--- stohastic/coef.cpp
+++ stohastic/coef.cpp
@@ -2,8 +2,17 @@
 #include <cmath>
 #include "coef.h"
 using namespace std;
+
+double pi = 3.14159265358979323846;
+
+
+
 double u(double x) {
-	double u1, u2;
+	double coef = 1.0/0.25;
+
+	// (-atan(x*coef) + pi/2) / pi * (3.0/4) + 1.4
+	return (-atan(x*coef) + pi/2) / pi * (3.0/4) + 1.0/4
+/*	double u1, u2;
 	u1=1.0;
 	u2=0.25*u1;
 	if (x<0.0) {
@@ -11,11 +20,12 @@ double u(double x) {
 	} else {
 		return u2;
 	}
+*/
 }
 
 double kappa(double x, double y) {
-	double kappa0 = 0.1;
-	return kappa0 * exp(y);
+	double kappa0 = 5;
+	return kappa0; //* exp(y);
 }
 
 double Qinj(double x, double dx, double y, double dy) {
--- stohastic/main.cpp
+++ stohastic/main.cpp
@@ -2,19 +2,20 @@
 #include <stdio.h>
 #include <math.h>
 #include <ctime>
-
+#include "coef.h"
 double a(double x, double y) {
-	return 0;
+	return u(x);
 }
 double b(double x, double y) {
-	return x;
+	return sqrt(2*kappa(x,y));
 }
 
 int main(int argc, char const *argv[]) {
+
 	set_g_rnd_seed(time(0));
 
 	double step = 0.1;
-	const int num = 100;
+	const int num = 500;
 	int lag = 1000;
 	FILE *out = fopen("ito.out", "w");
 
@@ -23,12 +24,14 @@ int main(int argc, char const *argv[]) {
 
 	double x = 1;
 	double t = 0; 
-
+	double * hist = new double[num];
 	fprintf(out, "%g\t%g\n", t, x);
 	for (int k = 1; k <= num; k++) {
 		for (int j = 0; j < lag; j++, t += dt) {
 			x += a(x, t)*dt + b(x,t)*rnd_g()*sqrt_dt;
 		}
+		hist[k] = x;
+	//	fprintf(out, "%g\n", x);
 		fprintf(out, "%g\t%g\n", t, x);
 		fflush(out);
 	}
